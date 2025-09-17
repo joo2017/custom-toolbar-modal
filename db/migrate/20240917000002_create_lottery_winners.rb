1938ad3837a6f24e7f92e88648bfff1d314467f1
joo2017 <30431754+joo2017@users.noreply.github.com>
2025-09-17 22:40:02 +0900

    Update 20240917000002_create_lottery_winners.rb

--- db/migrate/20240917000002_create_lottery_winners.rb
+++ db/migrate/20240917000002_create_lottery_winners.rb
@@ -3,15 +3,22 @@
 class CreateLotteryWinners < ActiveRecord::Migration[7.0]
   def change
     create_table :lottery_winners do |t|
+      # 这行会自动创建 lottery_event_id 并添加索引
       t.references :lottery_event, null: false, foreign_key: true
+      
+      # 这行会自动创建 user_id 并添加索引
       t.references :user, null: false, foreign_key: true
-      t.integer :floor_number, null: false # 中奖楼层
-      t.string :win_type, null: false # 'random' 或 'specific'
+      
+      t.integer :floor_number, null: false
+      t.string :win_type, null: false
       t.timestamps
     end
 
+    # 只保留这个复合唯一索引，因为它提供了 t.references 无法自动提供的功能
     add_index :lottery_winners, [:lottery_event_id, :user_id], unique: true
-  #  add_index :lottery_winners, :lottery_event_id
-  #  add_index :lottery_winners, :user_id
+    
+    # 下面这两行都因为 t.references 的存在而变得多余，可以删除
+    # add_index :lottery_winners, :lottery_event_id
+    # add_index :lottery_winners, :user_id
   end
 end
