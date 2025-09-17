db91d795633b006b43cd5dcbce89da23155efde9
joo2017 <30431754+joo2017@users.noreply.github.com>
2025-09-18 02:21:55 +0900

    Update init-custom-toolbar-modal.js

--- assets/javascripts/discourse/api-initializers/init-custom-toolbar-modal.js
+++ assets/javascripts/discourse/api-initializers/init-custom-toolbar-modal.js
@@ -1,21 +1,18 @@
 import { withPluginApi } from "discourse/lib/plugin-api";
+import LotteryFormModal from "discourse/plugins/custom-toolbar-modal/discourse/components/lottery-form-modal";
 
 export default {
   name: "init-custom-toolbar-modal",
-  
   initialize() {
     withPluginApi("0.8.31", (api) => {
       api.onToolbarCreate((toolbar) => {
         toolbar.addButton({
           title: "创建抽奖",
-          id: "lottery-button", 
-          group: "extras",
+          id: "lottery-button",
+          group: "extras", 
           icon: "gift",
           action: () => {
-            // 动态导入组件
-            import("../components/lottery-form-modal").then((module) => {
-              api.container.lookup("service:modal").show(module.default);
-            });
+            api.container.lookup("service:modal").show(LotteryFormModal);
           }
         });
       });
