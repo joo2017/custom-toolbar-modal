8777a1c7534e110ab68bb10c3000ad71872a86e5
joo2017 <30431754+joo2017@users.noreply.github.com>
2025-09-17 15:54:04 +0900

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -1,7 +1,13 @@
 # name: custom-toolbar-modal
-# about: 在编辑器工具栏添加按钮，弹出自定义模态框
+# about: 为编辑器工具栏添加自定义按钮，点击弹出包含表单的模态框
+# meta_topic_id: TODO
 # version: 0.0.1
 # authors: Your Name
-# url: https://github.com/yourname/custom-toolbar-modal
+# url: https://github.com/joo2017/custom-toolbar-modal
+# required_version: 2.7.0
 
 enabled_site_setting :custom_toolbar_modal_enabled
+
+after_initialize do
+  # 插件初始化后的服务器端代码
+end
