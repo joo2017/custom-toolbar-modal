1b43b602d970426057829ae3e86e91597137314d
joo2017 <30431754+joo2017@users.noreply.github.com>
2025-09-17 15:13:32 +0900

    Update custom-toolbar-modal.js

--- assets/javascripts/discourse/components/custom-toolbar-modal.js
+++ assets/javascripts/discourse/components/custom-toolbar-modal.js
@@ -1,24 +1,5 @@
-import { withPluginApi } from "discourse/lib/plugin-api";
-import CustomToolbarModal from "../components/custom-toolbar-modal"; // 注意相对路径
+import Component from "@glimmer/component";
 
-export default {
-  name: "custom-toolbar-modal",
-  initialize() {
-    withPluginApi("0.12.0", api => {
-      if (!api.container.lookup("site-settings:main").custom_toolbar_modal_enabled) return;
-
-      api.onToolbarCreate(toolbar => {
-        toolbar.addButton({
-          id: "custom_modal",
-          group: "extras",
-          icon: "plus", // 改成任何喜欢的icon
-          title: "Custom Modal",
-          perform: () => {
-            const modalService = api.container.lookup("service:modal");
-            modalService.show(CustomToolbarModal);
-          }
-        });
-      });
-    });
-  }
-};
+export default class CustomToolbarModal extends Component {
+  // 需要逻辑可在此处扩展
+}
